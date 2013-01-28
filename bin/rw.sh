83f4f1c9d0730f9d2ced24a996c8369d817f700a
Leonardo Thibes <leonardothibes@yahoo.com.br>
2013-01-28 09:21:29 -0200

    update

--- bin/rw.sh
+++ bin/rw.sh
@@ -4,12 +4,18 @@ DATE=$(date +%Y-%m-%d)
 ROOT=$(dirname $0)/../data
 ME=$(whoami)
 
-# Atribuindo permissão de escrita.
+# Atribuindo permissão de escrita ao diretório "data".
 for DIR in $(ls $ROOT)
 do
 	chmod 777 ${ROOT}/${DIR}
 done
-# Atribuindo permissão de escrita.
+# Atribuindo permissão de escrita ao diretório "data".
+
+# Atribuindo permissão de ecrita ao diretório de documentação.
+DOC=${ROOT}/../docs
+chmod 777 ${DOC}/PHPDoc
+chmod 777 ${DOC}/Tests
+# Atribuindo permissão de ecrita ao diretório de documentação.
 
 # Limpando diretório de LOGs.
 LOG=${ROOT}/logs
